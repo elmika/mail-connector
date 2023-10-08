730b1078a501da45916ab2e66454fcee577467a3
mika <1506612+elmika@users.noreply.github.com>
2023-10-08 17:44:29 +0200

    Connecting to generic mailbox.

--- README.md
+++ README.md
@@ -4,10 +4,18 @@ This is a python routine reading emails from an IMAP account.
 
 2. Then run the script:
 
+		`python3 read-google.py`
+		
+	or for a generic email (WIP, only connecting)
+		
 		`python3 read.py`
 
 
 
-Code is courtesy of 
+Code for reading in Google is courtesy of Geeks for Geeks:
 
 	`https://www.geeksforgeeks.org/python-fetch-your-gmail-emails-from-a-particular-user/`
+	
+Code for reading generic email is courtesy of The Python Code:
+
+`https://thepythoncode.com/article/reading-emails-in-python#:~:text=fetch()%20method%2C%20which%20fetches,()%20function%20from%20the%20email.`
--- read.py
+++ read.py
@@ -1,73 +1,28 @@
-# Importing libraries
-import imaplib, email
+import imaplib
+import email
+from email.header import decode_header
+import webbrowser
+import os
 
 from config import IMAP_URL, IMAP_USERNAME, IMAP_PASSWORD
 
-user = IMAP_USERNAME
+username = IMAP_USERNAME
 password = IMAP_PASSWORD
-imap_url = IMAP_URL
+imap_server = IMAP_URL
 
-from_filter = 'newsletters@squaredpoint.com'
+# clean text for creating a folder
+def clean(text):
+	return "".join(c if c.isalnum() else "_" for c in text)
 
-# Function to get email content part i.e its body part
-def get_body(msg):
-	if msg.is_multipart():
-		return get_body(msg.get_payload(0))
-	else:
-		return msg.get_payload(None, True)
+# create an IMAP4 class with SSL 
+imap = imaplib.IMAP4_SSL(imap_server)
+# authenticate
+imap.login(username, password)
 
-# Function to search for a key value pair 
-def search(key, value, con): 
-	result, data = con.search(None, key, '"{}"'.format(value))
-	return data
+status, messages = imap.select("INBOX")
+# number of top emails to fetch
+N = 3
+# total number of emails
+messages = int(messages[0])
 
-# Function to get the list of emails under this label
-def get_emails(result_bytes):
-	msgs = [] # all the email data are pushed inside an array
-	for num in result_bytes[0].split():
-		typ, data = con.fetch(num, '(RFC822)')
-		msgs.append(data)
-
-	return msgs
-
-# this is done to make SSL connection with GMAIL
-con = imaplib.IMAP4_SSL(imap_url) 
-
-# logging the user in
-con.login(user, password) 
-
-# calling function to check for email under this label
-con.select('Inbox') 
-
-# fetching emails from this user "tu**h*****1@gmail.com"
-msgs = get_emails(search('FROM', from_filter, con))
-
-# Uncomment this to see what actually comes as data 
-# print(msgs) 
-
-
-# Finding the required content from our msgs
-# User can make custom changes in this part to
-# fetch the required content he / she needs
-
-# printing them by the order they are displayed in your gmail 
-for msg in msgs[::-1]: 
-	for sent in msg:
-		if type(sent) is tuple: 
-
-			# encoding set as utf-8
-			content = str(sent[1], 'utf-8') 
-			data = str(content)
-
-			# Handling errors related to unicodenecode
-			try: 
-				indexstart = data.find("ltr")
-				data2 = data[indexstart + 5: len(data)]
-				indexend = data2.find("</div>")
-
-				# printing the required content which we need
-				# to extract from our email i.e our body
-				print(data2[0: indexend])
-
-			except UnicodeEncodeError as e:
-				pass
+print(messages)
